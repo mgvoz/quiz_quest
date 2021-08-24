5cbb08cbace0945fbdbdb09abfa0e3190b507acc
Marissa <marissagvozdenovich@gmail.com>
2021-08-24 16:28:57 -0400

    added functionality to check answer, fireworks for correct answer, next question button, incorrect answer alert

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,32 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import logo from './banner.png';
+import { Fireworks } from 'fireworks-js/dist/react';
 
 function Quiz() {
+	//set varibles
 	const [score, setScore] = useState(0);
 	const [questionNum, setQuestionNum] = useState(1);
 	const [questionList, setQuestionList] = useState([]);
-	const [questionIndex, setQuestionIndex] = useState(0);
+	const [startFireworks, setStartFireworks] = useState(false);
+	let selected;
 
+	//api call for questions
 	useEffect(() => {
-		fetch('https://opentdb.com/api.php?amount=15&category=9')
+		fetch('https://opentdb.com/api.php?amount=16&category=9')
 			.then((res) => res.json())
 			.then((data) => {
 				setQuestionList(data.results);
 			});
 	}, []);
 
-	const submitButton = (e) => {
-		e.preventDefault();
+	//fireworks animation
+	const options = {
+		speed: 5,
+		boundaries: {
+			x: 50,
+			y: 50,
+			width: '100%',
+			height: '100%',
+		},
+		delay: { min: 0, max: 10 },
+	};
+	const style = {
+		left: 0,
+		top: 0,
+		width: '100%',
+		height: '100%',
+		position: 'fixed',
+	};
+
+	//buttons
+	const answerChoices = document.getElementsByName('answer-choices');
+
+	const checkAnswer = () => {
+		for (var i = 0; i < answerChoices.length; i++) {
+			if (answerChoices[i].checked) {
+				selected = answerChoices[i].value;
+			}
+		}
+		if (selected === questionList[questionNum]?.correct_answer) {
+			setScore(score + 1);
+			setStartFireworks(true);
+		} else {
+			alert('Incorrect! Next question...');
+			nextQuestion();
+		}
+	};
+
+	const nextQuestion = () => {
+		setStartFireworks(false);
 		if (questionNum <= 14) {
 			setQuestionNum(questionNum + 1);
 		}
-		//check if answer correct, change score:
-		setScore(score + 1);
-		setQuestionIndex(questionIndex + 1);
 		//clear radio button selections
+		for (var j = 0; j < answerChoices.length; j++) {
+			answerChoices[j].checked = false;
+		}
 	};
 
-	console.log(questionList);
+	//SET UP END OF QUIZ AND SCORE PAGE, RANDOMIZE ANSWER CHOICES
 
 	return (
 		<div className='page-container'>
@@ -38,17 +79,21 @@ function Quiz() {
 						alt='quiz_quest logo'
 					></img>
 				</div>
+				{startFireworks && (
+					<Fireworks options={options} style={style} />
+				)}
 				<h1>General Knowledge Quiz</h1>
 				<div className='question-container'>
 					<h2>Question {questionNum}</h2>
-					<p>{questionList[questionIndex]?.question}</p>
-					<form onSubmit={submitButton}>
-						{questionList[questionIndex]?.incorrect_answers.map(
+					<p>{questionList[questionNum]?.question}</p>
+					<form>
+						{questionList[questionNum]?.incorrect_answers.map(
 							(ans, key) => {
 								return (
 									<div key={key}>
 										<input
 											name='answer-choices'
+											className='radio-buttons'
 											type='radio'
 											id={ans}
 											value={ans}
@@ -61,16 +106,30 @@ function Quiz() {
 						)}
 						<input
 							name='answer-choices'
+							className='radio-buttons'
 							type='radio'
-							id='option4'
-							value='option from db'
+							id={questionList[questionNum]?.correct_answer}
+							value={questionList[questionNum]?.correct_answer}
 						></input>
-						<label htmlFor='option4'>
-							{questionList[questionIndex]?.correct_answer}
+						<label
+							htmlFor={questionList[questionNum]?.correct_answer}
+						>
+							{questionList[questionNum]?.correct_answer}
 						</label>
 						<br />
-						<button type='submit' className='submit-button'>
-							submit
+						<button
+							onClick={() => checkAnswer()}
+							type='button'
+							className='check-answer-button'
+						>
+							check answer
+						</button>
+						<button
+							onClick={() => nextQuestion()}
+							type='button'
+							className='next-question-button'
+						>
+							next question
 						</button>
 					</form>
 				</div>
