0eb222258f43e2f412f53b93e56147762aaf2b5d
Marissa <marissagvozdenovich@gmail.com>
2021-08-29 14:33:16 -0400

    transferred question data to local storage to prevent unnecessary second reshuffling of answer choices

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,23 @@ function Quiz() {
 	const [questionList, setQuestionList] = useState([]);
 	const [startFireworks, setStartFireworks] = useState(false);
 	let selected;
+	const [loading, setLoading] = useState(true);
+
+	//set loading screen
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 3000);
+	}, []);
+
+	//randomize answer choices
+	const randomize = (array) => {
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			const temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+		return array;
+	};
 
 	//api call for questions
 	useEffect(() => {
@@ -16,8 +33,24 @@ function Quiz() {
 			.then((res) => res.json())
 			.then((data) => {
 				setQuestionList(data.results);
-			});
+				const questions = data.results.map((q) => `${q.question};`);
+				const answers = data.results.map(
+					(a) =>
+						`${randomize(
+							Array.of(
+								a.incorrect_answers,
+								a.correct_answer,
+							).flat(),
+						)}*`,
+				);
+				localStorage.setItem('questions', questions);
+				localStorage.setItem('answers', answers);
+			})
+			.catch((e) => console.log(e));
 	}, []);
+
+	const questions = localStorage.getItem('questions')?.split(';,');
+	const answers = localStorage.getItem('answers')?.split('*,');
 
 	//fireworks animation
 	const options = {
@@ -41,7 +74,8 @@ function Quiz() {
 	//buttons
 	const answerChoices = document.getElementsByName('answer-choices');
 
-	const checkAnswer = () => {
+	const checkAnswer = (e) => {
+		e.preventDefault();
 		for (var i = 0; i < answerChoices.length; i++) {
 			if (answerChoices[i].checked) {
 				selected = answerChoices[i].value;
@@ -54,7 +88,9 @@ function Quiz() {
 			if (questionNum === 15) {
 				alert('Incorrect! Your final score is...');
 			} else {
-				alert('Incorrect! Next question...');
+				alert(
+					`Incorrect! The correct answer was ${questionList[questionNum]?.correct_answer}. Next question...`,
+				);
 			}
 			nextQuestion();
 		}
@@ -78,123 +114,123 @@ function Quiz() {
 
 	const finalScore = (score / 15).toFixed(2).split('.')[1];
 
-	//RANDOMIZE ANSWER CHOICES
-
 	return (
-		<div className='page-container'>
-			<center>
-				<div className='logo-container'>
-					<img
-						className='logo-image'
-						src={logo}
-						alt='quiz_quest logo'
-					></img>
-				</div>
-				{questionNum <= 15 ? (
-					<>
-						{startFireworks && (
-							<Fireworks options={options} style={style} />
-						)}
-						<h1 className='quiz-page-heading'>
-							General Knowledge Quiz
-						</h1>
-						<p className='instructions'>
-							Instructions: Read the question and select your
-							answer. You must check your answer in order to earn
-							points!
-						</p>
-						<div className='question-container'>
-							<h2>Question {questionNum}</h2>
-							<p>{questionList[questionNum]?.question}</p>
-							<form>
-								{questionList[
-									questionNum
-								]?.incorrect_answers.map((ans, key) => {
-									return (
-										<div key={key}>
-											<input
-												name='answer-choices'
-												className='radio-buttons'
-												type='radio'
-												id={ans}
-												value={ans}
-												required
-											></input>
-											<label htmlFor={ans}>{ans}</label>
-											<br />
+		<>
+			{loading === false ? (
+				<div className='page-container'>
+					<center>
+						<div className='logo-container'>
+							<img
+								className='logo-image'
+								src={logo}
+								alt='quiz_quest logo'
+							></img>
+						</div>
+						{questionNum <= 15 ? (
+							<>
+								{startFireworks && (
+									<Fireworks
+										options={options}
+										style={style}
+									/>
+								)}
+								<h1 className='quiz-page-heading'>
+									General Knowledge Quiz
+								</h1>
+								<p className='instructions'>
+									Instructions: Read the question and select
+									your answer. You must check your answer in
+									order to earn points!
+								</p>
+								<div className='question-container'>
+									<h2>Question {questionNum}</h2>
+									<p>{questions[questionNum]}</p>
+									<form>
+										{answers[questionNum]
+											.split(',')
+											.map((ans) => {
+												return (
+													<div key={ans}>
+														<input
+															name='answer-choices'
+															className='radio-buttons'
+															type='radio'
+															id={ans}
+															value={ans}
+														></input>
+														<label htmlFor={ans}>
+															{ans}
+														</label>
+														<br />
+													</div>
+												);
+											})}
+										<div className='button-container'>
+											<button
+												onClick={(e) => checkAnswer(e)}
+												type='button'
+												className='check-answer-button'
+											>
+												check answer
+											</button>
+											<button
+												onClick={() => nextQuestion()}
+												type='button'
+												className='next-question-button'
+											>
+												{questionNum === 15
+													? 'see score'
+													: 'next question'}
+											</button>
+											<button
+												onClick={() =>
+													window.location.reload()
+												}
+												type='button'
+												className='check-answer-button'
+											>
+												start over
+											</button>
 										</div>
-									);
-								})}
-								<input
-									name='answer-choices'
-									className='radio-buttons'
-									type='radio'
-									id={
-										questionList[questionNum]
-											?.correct_answer
-									}
-									value={
-										questionList[questionNum]
-											?.correct_answer
-									}
-									required
-								></input>
-								<label
-									htmlFor={
-										questionList[questionNum]
-											?.correct_answer
-									}
-								>
-									{questionList[questionNum]?.correct_answer}
-								</label>
-								<br />
-								<div className='button-container'>
-									<button
-										onClick={() => checkAnswer()}
-										type='button'
-										className='check-answer-button'
-									>
-										check answer
-									</button>
-									<button
-										onClick={() => nextQuestion()}
-										type='button'
-										className='next-question-button'
-									>
-										{questionNum === 15
-											? 'see score'
-											: 'next question'}
-									</button>
+									</form>
 								</div>
-							</form>
-						</div>
-						<p className='score'>Correct answers: {score}</p>
-					</>
-				) : (
-					<>
-						<h1 className='quiz-page-heading'>
-							General Knowledge Quiz
-						</h1>
-						<Fireworks options={options} style={style} />
-						<div className='question-container'>
-							<center>
-								<h2>Quiz Complete!</h2>
-								<h3>Your score:</h3>
-								<h4>{score}/15</h4>
-								<h4>{finalScore}%</h4>
-							</center>
-						</div>
-						<button
-							onClick={() => window.location.reload()}
-							type='button'
-							className='start-over-button'
-						>
-							start over?
-						</button>
-					</>
-				)}
-			</center>
-		</div>
+								<p className='score'>
+									Correct answers: {score}
+								</p>
+							</>
+						) : (
+							<>
+								<h1 className='quiz-page-heading'>
+									General Knowledge Quiz
+								</h1>
+								<Fireworks options={options} style={style} />
+								<div className='question-container'>
+									<center>
+										<h2>Quiz Complete!</h2>
+										<h3>Your score:</h3>
+										<h4>{score}/15</h4>
+										<h4>
+											{score === 15
+												? '100%'
+												: finalScore + '%'}
+										</h4>
+									</center>
+								</div>
+								<button
+									onClick={() => window.location.reload()}
+									type='button'
+									className='start-over-button'
+								>
+									start over?
+								</button>
+							</>
+						)}
+					</center>
+				</div>
+			) : (
+				<h1 className='loading'>Loading your quiz...</h1>
+			)}
+		</>
 	);
 }
 
